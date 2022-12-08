f78b2d3a6801292138f936d5b5bdc7f59e6095cb
anilmirap <anilmirap@outlook.com>
2022-12-08 22:37:32 +0300

    Hi, Parmigiano

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, Parmigiano!
         </p>
         <a
           className="App-link"
