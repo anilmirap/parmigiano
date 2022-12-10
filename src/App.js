08d7b0c7d93f4d5e7bf94802dfabc6bf57162566
anilmirap <anilmirap@outlook.com>
2022-12-10 18:43:43 +0300

    setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import { Component } from "react";
+
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi, Parmigiano!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+      super();
+
+      this.state = {
+        name: 'Mozzarella',
+        company: 'Migros',
+      }
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi, {this.state.name}! I am being in sale at {this.state.company}.
+          </p>
+          <button 
+            onClick={() => {
+              this.setState({name: 'Parmigiano'});
+              this.setState({company: "Macro Center"});
+            }
+            }
+          >
+            Reply from Mozzarella
+          </button>
+        </header>
+      </div>
+    ); 
+  }
 }
 
 export default App;
